ad39ab7594d5c5989eb9b308b5b497486d6c1210
Marian <hellomariandev@gmail.com>
2023-02-22 20:08:41 +0100

    vite app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mastering React Context</h1>
+      <h2>
+        Understanding Managing State in React application with Props and
+        Context.
+      </h2>
+    </div>
+  );
+}
+
+export default App;
